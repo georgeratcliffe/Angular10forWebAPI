b3ee9cf0032d05e1cf98014267ecb46c464b261b
GEORGE W RATCLIFFE <george70fs@outlook.com>
2020-08-08 13:47:20 +0100

    add role based authorization

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,9 @@ import { UserComponent } from './user/user.component';
 import { RegistrationComponent } from './user/registration/registration.component';
 import { LoginComponent} from './user/login/login.component';
 import { HomeComponent } from './home/home.component';
-import { AuthGuard} from './auth/auth.guard'
+import { AuthGuard} from './auth/auth.guard';
+import { ForbiddenComponent } from './forbidden/forbidden.component';
+import { AdminPanelComponent } from './admin-panel/admin-panel.component';
 
 const routes: Routes = [
   {path:'', redirectTo:'user/login', pathMatch:'full'},
@@ -13,7 +15,9 @@ const routes: Routes = [
     { path:'registration', component: RegistrationComponent},
     { path:'login', component: LoginComponent}
   ]},
-  {path:'home', component:HomeComponent, canActivate:[AuthGuard]}
+  {path:'home', component:HomeComponent, canActivate:[AuthGuard]},
+  {path:'forbidden', component:ForbiddenComponent},
+  {path:'adminpanel', component:AdminPanelComponent, canActivate:[AuthGuard], data : {permittedRoles:['Admin']}}
 ];
 
 @NgModule({
--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -2,21 +2,32 @@ import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 import { Router } from '@angular/router';
+import { UserService } from 'src/app/shared/user.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
 
-  constructor(private router:Router) {
+  constructor(private router:Router, private service:UserService) {
 
   }
 
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot):  boolean {
-      if(localStorage.getItem('token') != null)
+      if(localStorage.getItem('token') != null){
+        let roles = next.data['permittedRoles'] as Array<string>;
+        if(roles){
+          if(this.service.roleMatch(roles)) return true;
+          else{
+            this.router.navigate(['/forbidden']);
+            return false;
+          }
+        }
+      
         return true;
+      }
       else
         this.router.navigate(['/user/login']);
         return false;
